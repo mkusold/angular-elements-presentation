462b2b1ee6193f1958523f32e5910e6a1ba85d67
M Kusold <kusrox@gmail.com>
2018-02-28 01:40:42 -0800

    vue js changes plus dist

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>VUE!</h1>
+    <div style="margin-left: 50%; margin-bottom:1em;">
+      <h4><img src="https://vuejs.org/images/logo.png"  
+        style="width: 50px; margin-bottom:-10px"/><span style="margin-bottom=5px">This is a Vue App</span></h4>
+    </div>
     <butter-passer v-bind:text="text"></butter-passer>
   </div>
 </template>
